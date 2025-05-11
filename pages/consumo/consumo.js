b4d3417d5d120a6ccdf58d98a23337cb2dd7caf5
caatnasc <caatnasc@gmail.com>
2025-05-11 02:42:41 +0000

    resolução do exercicio consumo

---
+++ pages/consumo/consumo.js
@@ -0,0 +1,12 @@
+function consumoMedio(distancia, combustivel) {
+    let consumo = distancia / combustivel
+    return consumo
+}
+
+let resultado1 = consumoMedio(500, 35.0)
+let resultado2 = consumoMedio(2254, 124.4)
+let resultado3 = consumoMedio(4554, 464.6)
+
+console.log(`${resultado1.toFixed(3)} km/l`)
+console.log(`${resultado2.toFixed(3)} km/l`)
+console.log(`${resultado3.toFixed(3)} km/l`)
